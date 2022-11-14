a18465770f71d6f6f1e27f6a44b00e666ee175b1
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-14 14:47:55 +0100

    JS finction form changing size of article

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-console.log('HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!')
+console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
+
+const article = document.querySelectorAll("article");
+const contents = document.querySelector(".article__content--js");
+const links = document.querySelectorAll(".article__link--js");
+
+
+article.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    const specialClass = panel.classList.toggle("article__special");
+contents.forEach(content => {
+    content.classList.toggle('.article__content-special');
+})
+  })});
