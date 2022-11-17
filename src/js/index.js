aa830d8bfee8d7b729be2373e1dc94406a324d98
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-17 14:39:24 +0100

    Head section filled, modules SCSS done

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,11 @@ console.log("HELLO. My name is Joanna. Nice to meet you on my website. Enjoy!");
 
 const articles = document.querySelectorAll(".article");
 
+function openSize() {
+  this.classList.toggle("open");
+  this.classList.toggle("open-active");
+}
 
-
-function openSize(){
-this.classList.toggle('open');
-this.classList.toggle('open-active');
-};
-
-
-articles.forEach(article=>{article.addEventListener('click', openSize)});
-
+articles.forEach((article) => {
+  article.addEventListener("click", openSize);
+});
