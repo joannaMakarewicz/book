4f616e9bdf9e346e67f615a90d9ecd9774b03e94
Joanna Makarewicz <joanna.milun@gmail.com>
2022-11-17 13:59:19 +0100

    Transform JS function changed

--- src/js/index.js
+++ src/js/index.js
@@ -8,15 +8,9 @@ const articles = document.querySelectorAll(".article");
 
 function openSize(){
 this.classList.toggle('open');
+this.classList.toggle('open-active');
 };
-
-function additionalElement(e){
-    console.log(e);
-    if(!e.propertyName.includes('flex')){
-    this.classList.toggle('open-active');
-    }
-}
 
 
 articles.forEach(article=>{article.addEventListener('click', openSize)});
-articles.forEach(article=>{article.addEventListener('transitionend', additionalElement)});
+
